00d4374fad3e5929d2442daf23d7a0758b465856
daydreaminger <ljqxnn@163.com>
2020-12-20 11:17:36 +0800

    fix:add app info fragment.

---
+++ app/src/main/java/com/daydreaminger/android/usagecontroller/ui/info/AppInfoFragment.java
@@ -0,0 +1,11 @@
+package com.daydreaminger.android.usagecontroller.ui.info;
+
+import com.daydreaminger.android.usagecontroller.ui.basic.AppBaseFragment;
+
+/**
+ * @author : daydreaminger
+ * @date : 2020/12/19 10:54
+ */
+public class AppInfoFragment extends AppBaseFragment {
+    private static final String TAG = "AppInfoFragment";
+}
